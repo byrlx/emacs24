dfe6ab76a1917ca93c8d792b85fb150b62db56f2
VuogNguyen <vuongnguyen1909@gmail.com>
2013-01-13 15:42:29 +0700

    message

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
   (list 'if running-xemacs (cons 'progn x)))
 (defmacro Xlaunch (&rest x)
   (list 'if (eq window-system 'x) (cons 'progn x)))
-;;;; /Macros ;;;;
+;;;; Macros ;;;;
 ;;; Setup initial emacs frames (windows) location and size
 ;; (setq default-frame-alist
   ;;    '((wait-for-wm . nil)
