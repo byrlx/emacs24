34ae1bb2bc16e2cd286011a077e9678a763f8601
VuogNguyen <vuongnguyen1909@gmail.com>
2013-01-13 23:16:33 +0700

    Message

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 
 
 ;;------------------------------------------------------------------------------
-;;; Unbinding Mouse click
+;;; Unbinding Mouse click and 4-arrow buttons
 (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]  
              [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
              [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
