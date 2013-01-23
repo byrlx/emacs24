72066491c6306a7057b54095435cbca708955255
VuogNguyen <vuongnguyen1909@gmail.com>
2013-01-23 18:02:51 +0700

    Org-mode

--- init.el
+++ init.el
@@ -376,3 +376,14 @@ file of a buffer in an external program."
 (ac-config-default)
 
 
+;;-----------------------------------------------------------------------------
+;;; Org-mode is a personal information management and outlining tool for Emacs
+(add-to-list 'load-path "/home/vuongnguyen/.emacs.d/org-mode")
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/school.org" 
+                             "~/org/home.org"))
