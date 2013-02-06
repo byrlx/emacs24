ce4b568147adf98b2d3f3e16aebcaac7d37405ae
VuogNguyen <vuongnguyen1909@gmail.com>
2013-02-06 20:14:06 +0700

    Update i-cycle

--- init.el
+++ init.el
@@ -203,10 +203,10 @@ smtpmail-debug-info t)
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
                           ("gnu" . "http://elpa.gnu.org/packages/")
                           ("marmalade" . "http://marmalade-repo.org/packages/")))
-;;(load "~/.emacs.d/site-lisp/icicles-install")
-;;(add-to-list 'load-path "~/.emacs.d/site-lisp/icicles")
-;;(require 'icicles)
-;;(icicle-mode 1)
+(load "~/.emacs.d/site-lisp/icicles-install")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/icicles")
+(require 'icicles)
+(icicle-mode 1)
 
 
 ;;------------------------------------------------------------------------------
