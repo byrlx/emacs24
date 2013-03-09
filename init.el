b0ee6e5b0bd1870bd356b497d0213e97b948fe4f
VuogNguyen <vuongnguyen1909@gmail.com>
2013-03-10 01:22:02 +0700

    Message

--- init.el
+++ init.el
@@ -374,34 +374,30 @@ file of a buffer in an external program."
 ;; ;;; TODO: find another way to bind key for ac-complete-clang only in c mode
 ;; (global-set-key (kbd "C-c C-?") 'ac-complete-clang)
 
-
-(add-to-list 'load-path "/home/vuongnguyen/.emacs.d/site-lisp")
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "/home/vuongnguyen/.emacs.d/site-lisp/ac-dict")
-
+(ac-config-default)
+(ac-set-trigger-key "<tab>")
+(global-auto-complete-mode t)
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/auto-complete-clang")	;add it to load-path
 (require 'auto-complete-clang)
-
-(setq ac-auto-start nil)
-(setq ac-quick-help-delay 0.5)
-(ac-set-trigger-key "<tab>")
-(defun my-ac-config ()
-(interactive)
-  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))
-(defun my-ac-cc-mode-setup ()
-(interactive)
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
-(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-;; ac-source-gtags
-(my-ac-config)
-
+(setq ac-clang-flags
+      (mapcar (lambda (item)(concat "-I" item))
+              (split-string
+               "
+ /usr/include/c++/4.6
+ /usr/include/c++/4.6/x86_64-linux-gnu/.
+ /usr/include/c++/4.6/backward
+ /usr/lib/gcc/x86_64-linux-gnu/4.6.1/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-linux-gnu/4.6.1/include-fixed
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+"
+               )))
+(setq clang-completion-suppress-error 't)
+(global-set-key (kbd "C-`") 'ac-complete-clang)
 ;;--------------------------------------------------------------------
 ;;; Org-mode is a personal information management and outlining tool for Emacs
 (add-to-list 'load-path "/home/vuongnguyen/.emacs.d/org-mode")
